9bff1543f4b8a1b3029b23a86f9ef5b473c4e879
= <=>
2021-06-02 23:57:02 +0200

    full roles logic working finally

--- src/server/middlewares/authRole.js
+++ src/server/middlewares/authRole.js
@@ -1,8 +1,41 @@
 const User = require('../models/db/userModel');
 const Role = require('../models/db/roleModel');
+const mongoose = require('mongoose');
 
 module.exports = {
     isAdmin: async(req, res, next) => {
+        const user = await User.findById(req.user._id);
+        // objectID
+        console.log(req.user._id);
+        console.log(user);
+        const rolesModel = await Role.find({ _id: { $in: user.roles } });
+
+        for (let i = 0; i < rolesModel.length; i++) {
+            if (rolesModel[i].name === "admin") {
+                next();
+                return;
+            }
+            // else {
+            //     res.redirect('/');
+            //     console.log('No eres admin');
+            // }
+        }
+        return res.status(403).json({ message: 'Sorry putito, only admins allowed' });
+    },
+    isModerator: async(req, res, next) => {
         const user = await User.findById(req.userId);
+        const roles = await Role.find({ _id: { $in: user.roles } });
+
+        for (let i = 0; i < roles.length; i++) {
+            if (roles[i].name === "moderator") {
+                next();
+                return;
+            }
+            // else {
+            //     res.redirect('/');
+            //     console.log('No eres admin');
+            // }
+        }
+        return res.status(403).json({ message: 'Sorry putito, only moderators allowed' });
     }
 }
--- src/server/routes/demoRoutes.js
+++ src/server/routes/demoRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const { isAdmin } = require('../middlewares/authRole');
+const { ensureAuthenticated, ensureGuest } = require('../middlewares/guard/authenticated');
+
 router.get('/', (req, res) => {
-    res.send('view demo')
+    res.render('demo/demo');
 })
 
-
-router.get('/create', (req, res) => {
-    res.send('Aqui solo puede entrar el admin, putitos not allowed');
+router.get('/create', ensureAuthenticated, isAdmin, (req, res) => {
+    res.render('demo/create');
+    // res.send('Aqui solo puede entrar el admin, putitos not allowed');
 })
 
 router.post('/create', (req, res) => {
